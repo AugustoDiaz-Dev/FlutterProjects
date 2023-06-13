5ab17c40322e215838ce719bff360d6923b78380
AugustoDiaz-Dev <augustodiaz_1@hotmail.com>
2023-06-13 17:39:53 +1000

    Quotes final

--- flutter_application_2/lib/quote_card.dart
+++ flutter_application_2/lib/quote_card.dart
@@ -3,7 +3,8 @@ import 'quote.dart';
 
 class QuoteCard extends StatelessWidget {
   final Quote quote;
-  QuoteCard({required this.quote});
+  final Function() delete;
+  QuoteCard({required this.quote, required this.delete});
 
   @override
   Widget build(BuildContext context) {
@@ -29,6 +30,11 @@ class QuoteCard extends StatelessWidget {
                   color: Colors.grey[800],
                 ),
               ),
+              SizedBox(height: 8),
+              TextButton.icon(
+                  onPressed: delete,
+                  label: Text('delete'),
+                  icon: Icon(Icons.delete))
             ],
           ),
         ));
